91ebcb5afb8ea0a045752cbd87ed699078c45c2b
Yogesh Khatri (@swiftforensics) <yogesh@swiftforensics.com>
2018-05-10 18:28:20 -0400

    High Sierra support for notifications

--- macNotifications.py
+++ macNotifications.py
@@ -15,15 +15,17 @@
 #  <http://www.gnu.org/licenses/>.
 # Script Name  : macNotifications.py
 # Author       : Yogesh Khatri
-# Last Updated : 1/4/2016
+# Last Updated : 5/10/2018
 # Requirement  : Python (2 or 3) and biplist
 #                biplist can be installed using the command 'pip install biplist' 
 # 
 # Purpose      : Parse the Notifications db found on mac OSX systems.
 #                This database in OSX 10.8 is located at:
 #                 /Users/<profile>/Library/Application Support/NotificationCenter/<GUID>.db
-#                On Yosemite and higher, it is located at:
-#                 /private/var/folders/<xx>/<yyyyyyy>/0/com.apple.notificationcenter/db
+#                On Yosemite, Sierra & El Capitan, it is located at:
+#                 /private/var/folders/<xx>/<yyyyyyy>/0/com.apple.notificationcenter/db/db
+#                On High Sierra, it is located at:
+#                 /private/var/folders/<xx>/<yyyyyyy>/0/com.apple.notificationcenter/db2/db
 #                   where xx and yyyyyyy are random and differ for each user and
 #                   installation of OSX
 # Usage        : macNotifications.py  <path_to_database>  <output_file.csv>
@@ -37,7 +39,7 @@
 #                   UUID    - Unique ID
 #                   Title   - Title of notification
 #                   Message - Text of message
-# Tested on    : OS X 10.8 - Mountain Lion to OS X 10.11 - El Capitan
+# Tested on    : OS X 10.8 - Mountain Lion to OS X 10.13 - High Sierra
 #
 # Send bugs and feedback to yogesh@swiftforensics.com
 #
@@ -47,7 +49,9 @@ import codecs
 import sqlite3
 import sys
 import os
+import uuid
 import biplist
+import datetime
 from biplist import *
 
 def RemoveTabsNewLines(str):
@@ -57,12 +61,85 @@ def RemoveTabsNewLines(str):
         pass
     return str
     
-#def GetSafeString(item):
+def ReadMacAbsoluteTime(mac_abs_time): # Mac Absolute time is time epoch beginning 2001/1/1
+    '''Returns datetime object, or empty string upon error'''
+    try:
+        return datetime.datetime.utcfromtimestamp(mac_abs_time + 978307200)
+    except:
+        pass
+    return ''
+
+def GetText(string_or_binary):
+    '''Converts binary or text string into text string. UUID in Sierra is now binary blob instead of hex text.'''
+    uuid_text = ''
+    try:
+        if type(string_or_binary) == buffer or type(string_or_binary) == bytes: # Python3 is 'bytes'
+            #hex_str = ''.join('{:02X}'.format(ord(x)) for x in string_or_binary)
+            uuid_text = str(uuid.UUID(bytes=string_or_binary)).upper()
+        else:
+            uuid_text = string_or_binary.upper()
+    except Exception as ex:
+        print('Error trying to convert binary value to hex text. Details: ' + str(ex))
+    return uuid_text
+
+def GetDbVersion(conn):
+    try:
+        cursor = conn.execute("SELECT value from dbinfo WHERE key LIKE 'compatibleVersion'")
+        for row in cursor:
+            print('db compatibleversion = {}'.format(row[0]))
+            return int(row[0])
+    except Exception as ex:
+        print("Exception trying to determine db version : " + str(ex))
+    return 15 #old version
+
+def Parse_ver_17_Db(conn, inputPath, outputPath):
+    '''Parse High Sierra's notification db'''
+    try:
+        conn.row_factory = sqlite3.Row
+        cursor = conn.execute("SELECT (SELECT identifier from app where app.app_id=record.app_id) as app, "\
+                                "uuid, data, presented, delivered_date FROM record")
+
+        try:
+            print ("Trying to create file '" + outputPath + "' for writing..")
+            with codecs.open(outputPath, 'w', encoding='utf-16') as csv:
+                csv.write ("Time\tShown\tBundle\tAppPath\tUUID\tTitle\tSubTitle\tMessage\r\n")
+                rowcount = 0
+                try:
+                    for row in cursor:
+                        rowcount += 1
+                        try:
+                            plist = readPlistFromString(row['data'])
+                            try:
+                                req = plist['req']
+                                title = RemoveTabsNewLines(req.get('titl', ''))
+                                subtitle = RemoveTabsNewLines(req.get('subt', ''))
+                                message = RemoveTabsNewLines(req.get('body', ''))
+                            except Exception as ex: lprint('Error reading field req - ' + str(ex))
+                        except (InvalidPlistException, NotBinaryPlistException, Exception) as e:
+                            print ("Invalid plist in table." + str(e) )
+                        try:
+                            csv.write ('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\r\n' %(ReadMacAbsoluteTime(row['delivered_date']), row['presented'], row['app'], '', GetText(row['uuid']), title, subtitle, message))
+                        except Exception as ex:
+                            print ("Error while writing to file, error details:\n", str(ex))
+                    print ("Finished processing! Wrote " + str(rowcount) + " rows of data.")                      
+                except Exception as ex:
+                    print ("Db cursor error while reading file " + inputPath)
+                    print(str(ex))
+        except Exception as ex:
+            print ("Failed to create file '" + outputPath + "' for writing. Is it locked? Perhaps a permissions issue")
+            print ("Error details: " , ex.args)
+    except Exception as ex:
+        print ("Sqlite error - \nError details: \n" + str(ex))
 
 def ProcessNotificationDb(inputPath, outputPath):
     try:
         conn = sqlite3.connect(inputPath)
         print ("Opened database successfully");
+
+        if GetDbVersion(conn) >= 17: # High Sierra
+            Parse_ver_17_Db(conn, inputPath, outputPath)
+            conn.close()
+            return
 
         conn.row_factory = sqlite3.Row
         cursor = conn.execute("SELECT datetime(date_presented + 978307200, 'unixepoch') as time_utc, "
@@ -111,7 +188,7 @@ def ProcessNotificationDb(inputPath, outputPath):
                     except (InvalidPlistException, NotBinaryPlistException, Exception) as e:
                         print ("Invalid plist in table.", e )
                     try:
-                        csv.write ('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\r\n' %(row['time'], row['shown'], row['bundle'], row['appPath'], row['uuid'], title, subtitle, message))
+                        csv.write ('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\r\n' %(ReadMacAbsoluteTime(row['time']), row['shown'], row['bundle'], row['appPath'], row['uuid'], title, subtitle, message))
                     except Exception as ex:
                         print ("Error while writing to file, error details:\n", ex.args)
                 print ("Finished processing! Wrote " + str(rowcount) + " rows of data.")
@@ -126,8 +203,10 @@ def ProcessNotificationDb(inputPath, outputPath):
 usage = ("macNotifications.py - Parse the OSX Notifications database \n\n"
          "This script parses the notification database found at \n"
          "/Users/<profile>/Library/Application Support/NotificationCenter/<GUID>.db\n"
-         " or for Yosemite onwards: \n"
-         "/private/var/folders/<xx>/<yyyyyyy>/0/com.apple.notificationcenter/db\n\n"
+         " or for Yosemite, Elcapitan, Sierra: \n"
+         "/private/var/folders/<xx>/<yyyyyyy>/0/com.apple.notificationcenter/db/db\n"
+         " or for High Sierra: \n"
+         "/private/var/folders/<xx>/<yyyyyyy>/0/com.apple.notificationcenter/db2/db\n\n"
          "Usage:\n"
          "macNotifications.py <path_to_db_file> <output.csv>\n"
          "Example: macNotifications.py  c:\\2676CFA4-F06E-4FFC-A48B-1C6457B2359D.db c:\\notifications.csv\n\n"
